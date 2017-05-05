c57192a102e8485a052209b0385fc1854993edcc
Arpit Goyal <arpit.go4@gmail.com>
2017-05-05 18:34:40 +0530

    margin at top added,

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -5,8 +5,10 @@ import Search from './Search.component';
 export default class App extends React.Component {
 	render() {
 		return (
-			<div className="container-fluid">
-				<Search />
+			<div className="app">
+				<div className="container-fluid">
+					<Search />
+				</div>
 			</div>
 		);
 	}
