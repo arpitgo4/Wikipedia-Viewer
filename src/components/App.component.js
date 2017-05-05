6e9a30b51f989c16a847da04194575eddae49535
Arpit Goyal <arpit.go4@gmail.com>
2017-05-05 16:39:36 +0530

    creating search icon. text added and styled

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Search from './Search.component';
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="container-fluid">
-				<div>
-					<h1 className="text-info bg-danger">Wikipedia Viewer</h1>
-				</div>
+				<Search />
 			</div>
 		);
 	}
---
+++ src/components/Search.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+	render() {
+		return (
+			<div className="row text-center">
+				<div className="col-xs-12">
+					<div className="row">
+						<div className="col-xs-12">
+							<p>Click here for a random article</p>
+						</div>
+					</div>
+
+					<div className="row search-icon-row">
+						<div className="col-xs-12">
+							<i className="search-icon center-block" />
+						</div>
+					</div>
+
+					<div className="row">
+						<div className="col-xs-12">
+							<p>Click icon to search</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
