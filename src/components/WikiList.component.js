58d88314ead61f6d0ce1aec14b27c0c472065474
Arpit Goyal <arpit.go4@gmail.com>
2017-05-09 12:39:16 +0530

    wikilist styling in progress

--- src/components/WikiList.component.js
+++ src/components/WikiList.component.js
@@ -7,14 +7,17 @@ export default class WikiList extends React.Component {
 		console.log('wiki pages', this.props.wikiList);
 		return (
 			<div className="row">
-				<div className="col-xs-offset-0 col-xs-11">
+				<div className="col-xs-offset-1 col-xs-10">
 					<ReactCSSTransitionGroup
 						transitionName="wikilist"
-						transitionEnterTimeout={200}
+						transitionEnterTimeout={500}
 						transitionLeaveTimeout={200}>
 						{
 							this.props.wikiList.map(page =>
-								<ul key={Math.random()}>{page.extract}</ul>)
+								<div className="wikiList-row" key={Math.random()}>
+									<p className="wikilist-row-title">{page.title}</p>
+									<p className="wikilist-row-body">{page.extract}</p>
+								</div>)
 						}
 					</ReactCSSTransitionGroup>
 				</div>
